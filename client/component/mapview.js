21f41527aee7fc115914ad3af24572495e83036b
Long Nguyen <caycham1@gmail.com>
2016-04-14 18:55:14 -0500

    get form to display on click

--- client/component/mapview.js
+++ client/component/mapview.js
@@ -1,26 +1,39 @@
 import React from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import mapURL from '../../config/component/config.js';
+import mapURL from '../../config/component/config';
 
 const MapView = (props) => {
   const handleClick = (thing) => {
-    console.log(thing);
+    props.onMapClick(thing);
   };
+  const markers = props.events.map((event) => {
+    let latlng = [event.lat, event.lng];
+    return (
+      <Marker key = {event.id} position={latlng}>
+        <Popup>
+          <span> {event.event} </span>
+        </Popup>
+      </Marker>
+    );
+  });
   const position = [37.77, -122.431297];
   return (
-    <Map onclick={handleClick} center={position} zoom={13} className="col-sm-8 MapView">
+    <Map onclick={handleClick} center={position} zoom={10} className="col-sm-8 MapView">
       <TileLayer
         url= {mapURL}
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> &copy; <a href="https://www.mapbox.com/map-feedback/">Mapbox</a>'
       />
-      <Marker position={position}>
-        <Popup>
-          <span> A pretty CSS3 popup.<br />Easily customizable. </span>
-        </Popup>
-      </Marker>
+      {markers}
     </Map>
   );
 };
 
 export default MapView;
 // render(map, document.getElementById('map-container'));
+
+//      <Marker position={position}>
+//        <Popup>
+//           <span> A pretty CSS3 popup.<br />Easily customizable. </span>
+//         </Popup>
+//       </Marker>
+
